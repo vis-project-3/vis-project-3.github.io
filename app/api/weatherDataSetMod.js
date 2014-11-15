89ab6cbfcf1400854fc78eb6b40b4156be5c9ef7
Ettore Trainti <ettore.trainiti@gmail.com>
2014-11-15 02:42:10 -0600

    WeatherBox Draft

---
+++ app/api/weatherDataSetMod.js
@@ -0,0 +1,53 @@
+
+/**
+ * Created by theja on 11/6/14.
+ */
+function weatherDataSetMod(){
+    //constructor code goes here
+    var self = this;
+    var key = '83a7051e2edc5e81';
+    var yahooURL = "https://query.yahooapis.com/v1/public/yql?q=";
+    var dataSetEndPoint = 'http://api.wunderground.com/api/' + key + '/';
+
+    this.getWeatherData = function(callBack){
+        var urlForDataSet = generateQuery();
+        $.ajax({
+            url: urlForDataSet,
+            dataType: "json",
+            success: function(data){
+                console.log(data);
+                var weatherData = modifyJSON(data);
+                console.log(weatherData)
+                callBack(weatherData);
+            }
+        });
+    }
+
+    var generateQuery = function(){
+
+        /*var requiredQuery = dataSetEndPoint;
+        //specify latitude and longitude
+        requiredQuery += 'conditions/';
+        requiredQuery += 'q/';
+        requiredQuery += 'IL/Chicago';
+        requiredQuery += '.json';
+
+        var yql = yahooURL;
+        yql += "select * from json where url='" + requiredQuery + "'" + "and itemPath=json.current_observation" +  "&format='json'";*/
+
+        var yql = "https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20json%20where%20url%3D'http%3A%2F%2Fapi.wunderground.com%2Fapi%2F83a7051e2edc5e81%2Fconditions%2Fq%2FIL%2FChicago.json'&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys"
+
+        return yql;
+    }
+
+    var modifyJSON = function(json){
+
+        var modifiedJSON = {
+            temp_c : json.query.results.json.current_observation.temp_c,
+            temp_f : json.query.results.json.current_observation.temp_f,
+            iconName: json.query.results.json.current_observation.icon,
+            condition : json.query.results.json.current_observation.weather
+        }
+        return modifiedJSON;
+    }
+}
---
+++ app/model/weatherIcons.js
@@ -0,0 +1,90 @@
+function weatherIcons() {
+
+    var path= "../resources/icons/weather/";
+
+    this.getIcon = function (kind) {
+
+        switch (kind) {
+            case "chanceflurries" :
+                return path + "cloudSnowFill.svg";
+
+            case "chancerain" :
+                return path + "cloudRainAltFill.svg";
+
+            case "chancesleet" :
+                return path + "cloudHailAlt.svg";
+
+            case "chancesnow" :
+                return path + "cloudSnowFill.svg";
+
+            case "chancetstorms" :
+                return path + "cloudLightning.svg";
+
+            case "clear" :
+                return path + "sun.svg";
+
+            case "cloudy" :
+                return path + "cloudFill.svg";
+
+            case "flurries" :
+                return path + "cloudHailAltFill.svg";
+
+            case "fog" :
+                return path + "cloudFogAlt.svg";
+
+            case "hazy" :
+                return path + "cloudFogFill.svg";
+
+            case "mostlycloudy" :
+                return path + "cloudSun.svg";
+
+            case "mostlysunny" :
+                return path + "cloudSun.svg";
+
+            case "partlycloudy" :
+                return path + "cloudSun.svg";
+
+            case "partlysunny" :
+                return path + "cloudSun.svg";
+
+            case "sleet" :
+                return path + "cloudHailAltFill.svg";
+
+            case "rain" :
+                return path + "cloudDrizzleFill.svg";
+
+            case "snow" :
+                return path + "cloudSnowAltFill.svg";
+
+            case "sunny" :
+                return path + "sun.svg";
+
+            case "tstorms" :
+                return path + "cloudLightningFill.svg";
+
+            case "thermo0" :
+                return path + "thermometer0.svg";
+
+            case "thermo25" :
+                return path + "thermometer25.svg";
+
+            case "thermo50" :
+                return path + "thermometer50.svg";
+
+            case "thermo75" :
+                return path + "thermometer75.svg";
+
+            case "thermo100" :
+                return path + "thermometer100.svg";
+
+            case "Celsius" :
+                return path + "Degrees-Celsius.svg";
+
+            case "Fahrenheit":
+                return path + "Degrees-Fahrenheit.svg"
+
+            default :
+                return path + "snowflake.svg";
+        }
+    }
+}
