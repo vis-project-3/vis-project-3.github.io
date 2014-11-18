706279719bf70c3a539e1ec2eba043cd1b283e1e
pvempa2@uic.edu <pvempa2@uic.edu>
2014-11-17 20:32:28 -0600

    Added UBER API access,uber image and test case

---
+++ app/api/uberDataSet.js
@@ -0,0 +1,111 @@
+/**
+ * Created by theja on 11/17/14.
+ */
+
+//TODO: Update the ORIGIN URI field in Uber App management to our app. Currently it is set to localhost.,i.e., http://localhost:8888
+function uberDataSet(){
+    //constructor code goes here
+    var self = this;
+    this.serverToken = '84qHw2f0sLvUw04IUcdqN3DW6H2FXt6MvJ5BJnOt';
+    this.clientId = 'SSyDGOO0xzPQHkhoHzU6TTichejAQAAJ';
+
+    //Gives lowest possible estimated price from (start_latitude,start_longitude) to (end_latitude,end_longitude)
+    //Sample Data Format
+    /* currency_code: "USD"
+     display_name: "uberX"
+     distance: 16.92
+     duration: 1780
+     estimate: "$21-29"
+     high_estimate: "29"
+     localized_display_name: "uberX"
+     low_estimate: "21"
+     product_id: "4bfc6c57-98c0-424f-a72e-c1e2a1d49939"
+     surge_multiplier: 1
+     */
+    this.getPriceEstimates = function(fromCoordinates,toCoordinates,callBack) {
+        $.ajax({
+            url: "https://api.uber.com/v1/estimates/price",
+            headers: {
+                Authorization: "Token " + self.serverToken
+            },
+            data: {
+                start_latitude: fromCoordinates.latitude,
+                start_longitude: fromCoordinates.longitude,
+                end_latitude: toCoordinates.latitude,
+                end_longitude: toCoordinates.longitude
+            },
+            success: function(result) {
+                var data = result["prices"];
+                if (typeof data != typeof undefined) {
+                    // Sort Uber products by time to the user's location
+                    data.sort(function (e0, e1) {
+                        return parseFloat(e0.low_estimate) - parseFloat(e1.low_estimate);
+                    });
+
+                    // Update the Uber button with the shortest time
+                    var shortest = data[0];
+                    if (typeof shortest != typeof undefined) {
+                        console.log("Updating price estimate...");
+                        $("#price").html("Low Estimate : $ " + shortest.low_estimate);
+                    }
+                }
+                callBack(result);
+            }
+        });
+    }
+
+    //Gives least possible estimated time for arrival of uber service for that latitude and longitude
+    //Sample Data Format
+    /*
+     display_name: "uberX"
+     estimate: 756
+     localized_display_name: "uberX"
+     product_id: "4bfc6c57-98c0-424f-a72e-c1e2a1d49939"
+     */
+    this.getTimeEstimates = function(fromCoordinates,callBack) {
+        $.ajax({
+            url: "https://api.uber.com/v1/estimates/time",
+            headers: {
+                Authorization: "Token " + self.serverToken
+            },
+            data: {
+                start_latitude: fromCoordinates.latitude,
+                start_longitude: fromCoordinates.longitude
+            },
+            success: function(result) {
+                var data = result["times"];
+                if (typeof data != typeof undefined) {
+                    // Sort Uber products by time to the user's location
+                    data.sort(function (t0, t1) {
+                        return t0.estimate - t1.estimate;
+                    });
+                    // Update the Uber button with the shortest time
+                    var shortest = data[0];
+                    if (typeof shortest != typeof undefined) {
+                        console.log("Updating time estimate...");
+                        $("#time").html("IN " + Math.ceil(shortest.estimate / 60.0) + " MIN");
+                    }
+                }
+                callBack(result);
+            }
+        });
+    }
+
+    this.getAvailableUberProducts = function(fromCoordinates,callBack) {
+        $.ajax({
+            url: "https://api.uber.com/v1/products",
+            headers: {
+                Authorization: "Token " + self.serverToken
+            },
+            data: {
+                start_latitude: fromCoordinates.latitude,
+                start_longitude: fromCoordinates.longitude,
+            },
+            success: function(result) {
+                callBack(result);
+            }
+        });
+    }
+}
+
+
