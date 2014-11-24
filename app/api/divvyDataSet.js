46d61fe32df64e1c34b23def8e0e53ee99e3f9b8
Ettore Trainti <ettore.trainiti@gmail.com>
2014-11-24 02:40:42 -0600

    Added Divvy Layer Data

---
+++ app/api/divvyDataSet.js
@@ -0,0 +1,37 @@
+function divvyDataSet() {
+    var entryPoint = "http://www.divvybikes.com/stations/json";
+
+    var generateQuery = function (coords) {
+        var query = entryPoint;// + parameters;
+        //var southWest = coords.getSouthWest();
+        //var northEast = coords.getNorthEast();
+
+        //console.log(southWest);
+        //console.log(northEast);
+        //var parameters = "";
+
+        //parameters += " and latitude>=" + southWest.lat;
+        //parameters += " and longitude>=" + southWest.lng;
+        //parameters += " and latitude<=" + northEast.lat;
+        //parameters += " and longitude>=" + northEast.lng;
+
+        return 'http://query.yahooapis.com/v1/public/yql?format=json&q=' + encodeURIComponent('select * from json where url="' + entryPoint + '"');
+    };
+
+    var getData = function (url, callback) {
+        console.log(url);
+        $.ajax({
+            url: url,
+            success: function (data) {
+                console.log(data);
+                callback(data.query.results.json.stationBeanList);
+            }
+        });
+    };
+
+    this.getStations = function (coords, callback) {
+        var query = generateQuery(coords);
+        getData(query, callback);
+    };
+
+}
--- app/controller/controllerDivvy.js
+++ app/controller/controllerDivvy.js
@@ -6,25 +6,25 @@ function controllerDivvy(mapObject) {
         .id("divvy-layer")
         .iconPath("resources/icons/icon-divvy.svg");
 
-    // var layer = new genericLayer();
-    // layer.setKey("id");
-    // layer.setIcon("divvy");
-    //
-    // var popup = new popupCrimes();
-    // layer.setPopup(popup);
-    //
-    // controller.layer(layer);
-    //
-    // var query = function() {
-    //     var fromDate = d3.time.day.offset(new Date(), -30);
-    //
-    //     return chicagoQuery()
-    //     .setEndPoint("ijzp-q8t2.json?")
-    //     .dateColumn("date")
-    //     .fromDate(fromDate);
-    // }
-    //
-    // controller.query(query);
+    var layer = new genericLayer();
+        layer.setKey("id");
+        layer.setIcon("divvy");
+
+    var popup = new popupDivvy();
+    layer.setPopup(popup);
+
+    controller.layer(layer);
+
+    var api = new divvyDataSet();
+
+    var getData = function(bounds, callback) { // L.latLngBounds
+        api.getStations(bounds, callback);
+    };
+
+    controller.dataCallback(getData);
+
+    controller.latitudeAccessor(function(d){return d.latitude});
+    controller.longitudeAccessor(function(d){return d.longitude});
 
     this.get = function() { return controller };
 
