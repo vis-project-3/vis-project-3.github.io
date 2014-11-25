33e4366d5ce0a8861b6b60de1815578c27171fe3
Paul Murray <plmrry@gmail.com>
2014-11-24 20:03:29 -0600

    fix popup date

--- app/view/popupPotholes.js
+++ app/view/popupPotholes.js
@@ -1,9 +1,11 @@
 function popupPotholes(){
 
     this.generatePopupContent = function(data){
+        d3
         var iconObject = new iconsApp();
-        var icon = "<p><img src=\"" + iconObject.getIcon("pothole") + "\"/></p>"
-        var creationDate  = "<h3><b>Signaled on : </b>" + data.creation_date + "</h3>";
+        var icon = "<p><img width=\"40\" height=\"40\" src=\"" + iconObject.getIcon("pothole") + "\"/></p>";
+        var format = d3.time.format("%x");
+        var creationDate  = "<h3><b>Reported on : </b>" + format(new Date(data.creation_date)) + "</h3>";
 
         return icon + creationDate;
     }
